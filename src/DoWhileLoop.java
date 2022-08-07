6b179080bb014325acd1b0490fd40a311d028df3
Gekon <grzegorz.szustkiewicz@gmail.com>
2022-08-07 10:33:24 +0200

    add DoWhileLoop

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+    public static void main(String[] args){
+
+        int number = 0;
+        do {
+            System.out.println("Moja wartosc to: " + number + " i jestem mniejsza niz 20");
+            number++;
+        }
+        while (number < 20);
+    }
+}
