b4e3fe005299d89e3dae0553bbe64e6d6c6fad7f
Gekon <grzegorz.szustkiewicz@gmail.com>
2022-08-07 10:12:16 +0200

    add PetlaFor

---
+++ src/PetlaFor.java
@@ -0,0 +1,19 @@
+public class PetlaFor {
+    public static void main(String[] args) {
+
+        /*for (int i=0; i<100; i=i+2) {
+            System.out.println(i);
+        }*/
+
+        /*for (int i=0; i<100; i++) {
+            if(i%5==0) {
+                System.out.println(i);
+            }
+        }*/
+
+        for (int j=0; j<100; j++) {
+            System.out.println("Kocham Cie Grzesiu! :)");
+        }
+
+    }
+}
