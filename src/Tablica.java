07c335dcf9216819c6e9cee06944162063e3513b
Gekon <grzegorz.szustkiewicz@gmail.com>
2022-08-08 09:47:54 +0200

    Add Tablica

--- src/Tablica.java
+++ src/Tablica.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Tablica {
+public class Tablica {
+
+    public static void main(String[] args){
+       /* String[] imiona = new String[3];
+        imiona[0] = "Geguś";
+        imiona[1] = "Domi";
+        imiona[2] = "Witek";
+
+        System.out.println(imiona[0]);
+        System.out.println(imiona[1]);
+        System.out.println(imiona[2]);*/
+
+        int[] lottoNumbers = new int[] {1,2,3,4,5,6};
+        /* System.out.println(lottoNumbers[0]);
+        System.out.println(lottoNumbers[1]);
+        System.out.println(lottoNumbers[2]);
+        System.out.println(lottoNumbers[3]);
+        System.out.println(lottoNumbers[4]);
+        System.out.println(lottoNumbers[5]);
+
+        System.out.println(lottoNumbers.length);*/
+
+      /*  for (int i=0; i<lottoNumbers.length; i++){
+            System.out.println(lottoNumbers[i]);*/
+
+        System.out.println(lottoNumbers[7]);
+
+
+
+
+
+    }
 }
